5cd2576b221bef77d9137b6dcb9f2f220f809544
vaishnaray1 <vaishnavi.narayanan@chicagobooth.edu>
2022-04-11 19:28:59 +0000

    update

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,10 @@
 class Director < ApplicationRecord
+  validates :dob, presence: true
+  validates :name, presence: true
+  validate :dob_greater_than_today
+
+  def dob_greater_than_today
+      errors.add(:dob, "DOB cannot be in the future.") if
+        !dob.blank? and dob > Date.today
+    end
 end
