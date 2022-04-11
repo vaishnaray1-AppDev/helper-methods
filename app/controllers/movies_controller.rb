ed5cff7a2d083c874b34a3f9dfac37f2b257e2c2
vaishnaray1 <vaishnavi.narayanan@chicagobooth.edu>
2022-04-11 01:40:20 +0000

    Updated controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,9 +26,14 @@ class MoviesController < ApplicationController
   end
 
   def create
+    movie_attributes = params.require(:movie).permit(:title, :description)
+
+    @movie = Movie.new(movie_attributes)
+
+
     @movie = Movie.new
-    @movie.title = params.fetch("title")
-    @movie.description = params.fetch("description")
+    @movie.title = params.fetch(:movie).fetch(:title)
+    @movie.description = params.fetch(:movie).fetch(:description)
 
     if @movie.valid?
       @movie.save
@@ -45,16 +50,22 @@ class MoviesController < ApplicationController
   end
 
   def update
-    movie = Movie.find( params.fetch(:id))
 
-    movie.title = params.fetch("title")
-    movie.description = params.fetch("description")
+    movie_attributes = params.require(:movie).permit(:title, :description)
 
-    if movie.valid?
-      movie.save
-      redirect_to movie_url(the_movie),  notice: "Movie updated successfully." 
+    @movie = Movie.new(movie_attributes)
+
+
+    @movie = Movie.new
+    @movie.title = params.fetch(:movie).fetch(:title)
+    @movie.description = params.fetch(:movie).fetch(:description)
+
+    if 
+      @movie.valid?
+      @movie.save
+      redirect_to movie_url(@movie),  notice: "Movie updated successfully." 
     else
-      redirect_to movie_url(the_movie),  alert: "Movie failed to update successfully." 
+      redirect_to movie_url(@movie),  alert: "Movie failed to update successfully." 
     end
   end
 
